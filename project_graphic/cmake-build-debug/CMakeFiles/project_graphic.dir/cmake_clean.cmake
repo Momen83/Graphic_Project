299377e67e772cb7d66727fe059f9d2df24aaf35
Momen83 <77742840+Momen83@users.noreply.github.com>
2022-05-28 21:04:16 +0200

    Add files via upload

---
+++ project_graphic/cmake-build-debug/CMakeFiles/project_graphic.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_graphic.dir/main.cpp.obj"
+  "libproject_graphic.dll.a"
+  "project_graphic.exe"
+  "project_graphic.exe.manifest"
+  "project_graphic.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_graphic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
